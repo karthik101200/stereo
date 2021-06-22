b0527439dd83d8ff3419aa064d050da86ec6f069
karthik101200 <64783286+karthik101200@users.noreply.github.com>
2021-06-22 10:41:44 +0530

    Update stere0_reconstruction.py

--- stere0_reconstruction.py
+++ stere0_reconstruction.py
@@ -5,30 +5,40 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 
 
-
+#READS IMAGES
 imgL=cv2.imread('bike_l.png')
 imgR=cv2.imread('bike_r.png')
 #cv2.destroyAllWindows()
 
-#print(image.shape)
+
+#PARAMETERS FOR DISPARITY MAP-FOLLOW calib.txt
 doffs=124.343
 b=193.001
 f=3979.911
-window_size2=5
+window_size2=5 
+
+#COMPUTES DISPARITY
 stereo =  cv2.StereoSGBM_create(minDisparity = 23, numDisparities = 222, blockSize = 5, uniquenessRatio = 5, speckleWindowSize = 5, speckleRange = 5, disp12MaxDiff = 0, P1 = 8*3*window_size2, P2 = 32*3*window_size2)
 disp_map = stereo.compute(imgL,imgR).astype(np.float32)/240
-disparity = cv2.pyrDown(disp_map)
 
+#RESIZING DISPARITY(NOT NEEDED IF A LARGE POINT CLOUD IS OKAY-SEE O/P WITH BETTER CLARITY IN DRIVE)
+disparity = cv2.pyrDown(disp_map)
+#RESIZING IMAGE FOR COLOUR(NOT NEEDED IF A LARGE POINT CLOUD IS OKAY-SEE O/P WITH BETTER CLARITY IN DRIVE)
 image = cv2.pyrDown(cv2.imread('bike_l.png'))
-distortion=np.zeros((5,1))
-k=np.array([[3979.911, 0, 1244.772] ,[0, 3979.911 ,1019.507], [0 ,0 ,1]])
-T=np.zeros((3,1))
-T[0][0]=b
+
+# distortion=np.zeros((5,1))
+# k=np.array([[3979.911, 0, 1244.772] ,[0, 3979.911 ,1019.507], [0 ,0 ,1]])
+# T=np.zeros((3,1))
+# T[0][0]=b
 # R1,R2,P1,P2,Q,_,_ = cv2.stereoRectify(k,distortion,k,distortion,(2964,2000),np.identity(3),T)
+
+#DEFINING Q FOR REPROJECTION BY FOLLOWING calib.txt
 Q = np.array([[1, 0, 0, -2964/2], [0, 1, 0, -2000/2],[0, 0, 0, f],[0, 0, -1/b, doffs/b]])
+
+#FUNCTION TO OBTAIN THE POINT CLOUD
 points = cv2.reprojectImageTo3D(disparity, Q)
 
-
+#OUTLIERS REMOVAL
 mask = disparity > disparity.min()
 xp=points[:,:,0]
 yp=points[:,:,1]
@@ -45,9 +55,6 @@ point3d=np.hstack((xp,yp,zp))
 
 #print(mask[0])
 colors = image
-#cv2.imshow('colors', colors)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
 
 points = points
 colors = image
@@ -56,6 +63,7 @@ points = points.reshape(-1,3)
 colors = colors.reshape(-1,3)
 points = np.hstack([points, colors])
 
+#MAKING A PLY FILE FOR VISUALISATION
 ply_header = '''ply
 	format ascii 1.0
 	element vertex %(vert_num)d
@@ -73,6 +81,6 @@ with open('output.ply', 'w') as f:
 
 print("DONE")
 
-cv2.imshow('window', image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
